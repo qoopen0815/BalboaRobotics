2683d5dbd4c553548f30fe6cf5abbfac57aa69c3
calm0815(kuwano) <ku_skyline_wa@yahoo.co.jp>
2019-07-04 08:19:04 +0900

    edit something

--- Balboa32U4_MATLAB/param/linear_model.m
+++ Balboa32U4_MATLAB/param/linear_model.m
@@ -1,41 +1,36 @@
 clear
-
 balboa_param;
 
-% 変数(連続系)
+% 変数
 a11 = (m_w + m_p)*power(r_w, 2) + 2*m_p*r_w*r_p + m_p*power(r_p, 2) + I_p + I_w;
 a12 = (m_w + m_p)*power(r_w, 2) + m_p*r_w*r_p + I_w;
 a21 = (m_w + m_p)*power(r_w, 2) + m_p*r_w*r_p + I_w;
 a22 = (m_w + m_p)*power(r_w, 2) + I_w + power(n, 2)*I_m;
-
 delta = a11*a22 - a12*a21;
 
-% 状態方程式(連続系)
+% 状態方程式（連続系）
 sys_A = [0 1 0 0;
          (a22*m_p*g*r_p)/delta 0 0 (a12*power(n, 2)*k_t*k_b/R)/delta;
          0 0 0 1;
          (-a21*m_p*g*r_p)/delta 0 0 (-a11*power(n, 2)*k_t*k_b/R)/delta];
-
 sys_B = [0;
          (-a12*n*k_t/R)/delta;
          0;
          (a11*n*k_t/R)/delta];
-
-sys_C = [0 1 1 0];
-sys_D = 0;
+sys_C = diag([0 1 1 0]);
+sys_D = [0; 0; 0; 0];
 
 % 離散化
 syms Ts real
-Ts = 0.01;
-sys_Ad = eye(4) + sys_A * Ts;
-sys_Bd = sys_B * Ts;
+Ts = 0.01; % [sec]
+% sys_Ad = eye(4) + sys_A * Ts;
+% sys_Bd = sys_B * Ts;
+[sys_Ad, sys_Bd] = c2d(sys_A, sys_B, Ts);
 
-% 状態空間表現
-sys = ss(sys_A, sys_B, sys_C, sys_D)
-% 状態空間表現の係数の抽出
-[sys_A, sys_B, sys_C, sys_D] = ssdata(sys)
+% 状態空間表現（連続系）
+sys = ss(sys_A, sys_B, sys_C, sys_D);
+% 状態空間表現（離散系）
+sysd = ss(sys_Ad, sys_Bd, sys_C, sys_D, Ts);
 % 状態空間表現から伝達関数表現（零点/極/ゲイン型）へ
-zpk_P = zpk(sys)
-% 離散化
-Ts = 0.01; % 周期[sec]
-sysd = c2d(sys, Ts, 'zoh');
+zpk = zpk(sys);
+% zpkd = zpk(sysd)

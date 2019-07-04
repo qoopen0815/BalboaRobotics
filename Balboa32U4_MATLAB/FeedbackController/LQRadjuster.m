55131616323263b47af09244d9a68b35f9efe016
calm0815(kuwano) <ku_skyline_wa@yahoo.co.jp>
2019-07-04 12:08:12 +0900

    オブザーバ構築

--- Balboa32U4_MATLAB/FeedbackController/LQRadjuster.m
+++ Balboa32U4_MATLAB/FeedbackController/LQRadjuster.m
@@ -1,16 +1,13 @@
-% LQRƒpƒ‰ƒ[ƒ^
 clear
-
 balboa_param;
 linear_model;
 
-% x_ref = [0; 0; 0; 10];
+L = diag([0.7 0.5 0.5 0.5]);
 
+% LQR optimization
 Q = diag([10 1 1 1]);
 R = 1e+2;
-
 K  = -lqr(sys_A, sys_B, Q, R);
 Kd = -dlqr(sys_Ad, sys_Bd, Q, R);
-L = diag([0.5 0.5 0.5 0.5]);
 
 x0 = [20*pi/180;0;0;0];

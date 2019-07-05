876e2e37438434b4a3b677a94e667436cf711a3f
calm0815(kuwano) <ku_skyline_wa@yahoo.co.jp>
2019-07-05 20:08:45 +0900

    observer構築完了（未設計）

--- Balboa32U4_MATLAB/FeedbackController/LQRadjuster.m
+++ Balboa32U4_MATLAB/FeedbackController/LQRadjuster.m
@@ -2,12 +2,12 @@ clear
 balboa_param;
 linear_model;
 
-L = diag([0.7 0.5 0.5 0.5]);
+L = [0.25; 0.75; -0.15; 0.35];
 
 % LQR optimization
 Q = diag([10 1 1 1]);
 R = 1e+2;
-K  = -lqr(sys_A, sys_B, Q, R);
-Kd = -dlqr(sys_Ad, sys_Bd, Q, R);
+K  = lqr(sys_A, sys_B, Q, R);
+Kd = dlqr(sys_Ad, sys_Bd, Q, R);
 
 x0 = [20*pi/180;0;0;0];

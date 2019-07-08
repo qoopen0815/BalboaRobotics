4d5f9eb93e367b5dc84f75c550f17f4f4ab28839
calm0815 <ku_skyline_wa@yahoo.co.jp>
2019-07-09 01:54:00 +0900

    move & split code

---
+++ Balboa32U4/Arduino/FeedbackController/include/conversion.h
@@ -0,0 +1,10 @@
+#define PI 3.14159
+
+//convert encoder val to wheel deg
+float en2deg(float encoder);
+
+//convert deg to rad
+float deg2rad(float deg);
+
+//convert rad to deg
+float rad2deg(float rad);

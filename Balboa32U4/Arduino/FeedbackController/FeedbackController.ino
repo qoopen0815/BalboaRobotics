9fc2340ae5756d116073f41e232f884196d7d3c4
calm0815 <ku_skyline_wa@yahoo.co.jp>
2019-07-13 23:48:17 +0900

    test

--- Balboa32U4/Arduino/FeedbackController/FeedbackController.ino
+++ Balboa32U4/Arduino/FeedbackController/FeedbackController.ino
@@ -1,7 +1,13 @@
+#include <Wire.h>
 #include <ArduinoSTL.h>
+#include <LSM6.h>
 #include "matrix.h"
 
+const int Ts = 10.0;  //周期Ts [ms]
+
 MatrixFunction MatrixFunction;
+LSM6 imu;
+float deg, gy;
 
 std::vector<std::vector<float>> sys_Ad = {
     {1.00261874869926, 0.0100091341522741, 0.0, 0.00337554714878429},
@@ -41,12 +47,61 @@ std::vector<std::vector<float>> estimatedState(std::vector<std::vector<float>> o
     return State;
 }
 
+void setupLSM6(void)
+{
+    if (!imu.init())
+    {
+        // Failed to detect the LSM6.
+        // ledRed(1);
+        while(1)
+        {
+            Serial.println(F("Failed to detect the LSM6."));
+            delay(100);
+        }
+    }
+    imu.enableDefault();
+    // Set the gyro full scale to 500 dps
+    imu.writeReg(LSM6::CTRL2_G, 0b10000100);
+    // Set the accelerometer full scale to 8 g
+    imu.writeReg(LSM6::CTRL1_XL, 0b10001111);
+    imu.calibrateIMU();
+}
+
+void setupIMU(void)
+{
+    setupLSM6();
+}
+
+void updateIMU(void)
+{
+    imu.read();
+
+    // 角速度値を分解能で割って角速度(degrees per sec)に変換する
+    gy = (imu.g.y-imu.cg.y) / 17.5;  // FS_G 500 dps => 57.1 LSB/dps [17.50 mdps/LSB]
+}
+
+static unsigned long startTime = 0;
+static unsigned long goalTime = 0;
+static unsigned long processTime = 0;
+
 void setup()
 {
     Serial.begin(9600);
+    Wire.begin();
+    setupIMU();
+
+    deg = 0;
+    gy = 0;
 }
 
 void loop()
 {
-    delay(1000);
+    startTime = millis();
+
+    updateIMU();
+    deg += gy * Ts / 1000;
+    Serial.println(deg);
+
+    goalTime = millis();
+    delay(Ts - (goalTime - startTime));
 }
